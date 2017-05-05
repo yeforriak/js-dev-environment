2d086a95128a648ecab754dc3412566fbb1e319f
Victor <yeforriak@gmail.com>
2017-05-05 17:41:18 +0100

    Add user api that fakes connection to db

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-import './index.css'
-import numeral from 'numeral';
+import {getUsers} from './api/userApi'
 
-const numeralValue = numeral(1000).format('$0,0.00');
 
-console.log(`I would pay ${numeralValue}!`);
+getUsers().then(result => {
+  let usersBody = "";
+  result.forEach(user => {
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
